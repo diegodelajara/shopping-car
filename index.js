73f3f9d71e4a3c3f31e4145965a7859afdaa0d84
Diego De la Jara <diego.delajara@apiservice.cl>
2021-04-14 00:56:49 -0400

    :sparkles: Add Shopping-car

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 
+function updateCount() {
+  const total = document.querySelectorAll('.shopping-car-product').length
+  document.querySelector('.badge').innerText = total
+}
+
+function showHideList(style) {
+  document.getElementById('products-added').style.display = style
+}
+
+function removeItem(item) {
+  console.log('Eliminar ', item);
+}
+
+function addItem(item) {
+  const list    = document.getElementById('products-added')
+  const div     = document.createElement('div')
+  const span    = document.createElement('span')
+  const button  = document.createElement('button')
+  span.innerText = item.name
+  button.innerText = '-'
+  button.onclick = function(){
+    removeItem(item)
+  }
+  div.className = 'shopping-car-product'
+  div.appendChild(span)
+  div.appendChild(button)
+  list.appendChild(div)
+  updateCount()
+}
+
 function getProducts() {
   const url = 'https://my.api.mockaroo.com/products.json?key=66cff0f0';
 
@@ -12,11 +42,16 @@ function getProducts() {
   .then(function(data) {
     const parent = document.getElementById('product-list')
     return data.map(product => {
-      const article = document.createElement('article')
-      const imgContainer = document.createElement('div')
-      const img     = document.createElement('img')
-      const name    = document.createElement('span')
-      const price   = document.createElement('span')
+      const article       = document.createElement('article')
+      const imgContainer  = document.createElement('div')
+      const img           = document.createElement('img')
+      const name          = document.createElement('span')
+      const price         = document.createElement('span')
+      const addButton     = document.createElement('button')
+      addButton.innerText = 'Añadir al carrtito '
+      addButton.onclick = function(){
+        addItem(product)
+      }
       article.className = 'product'
       imgContainer.className = 'img-container'
       imgContainer.setAttribute(
@@ -29,13 +64,19 @@ function getProducts() {
       article.appendChild(imgContainer)
       article.appendChild(name)
       article.appendChild(price)
+      article.appendChild(addButton)
       parent.appendChild(article)
     })
   })
   .catch(error => console.error('Error:', error))
 }
 
-const products = getProducts()
+document.querySelector('body').addEventListener('click', function(e) {
+  e.target.id === 'shopping-car' || e.target.localName === 'button'
+  ? showHideList('block')
+  : showHideList('none')
+})
+getProducts()
 // if (products) {
   
 //   for (let i = 0; i < products.length; i++) {
